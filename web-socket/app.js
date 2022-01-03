6aa5920bbbdc9c3df6c7aabeb540b89eab021606
Gonzalo Cavazos <gonzalo.adriancr@gmail.com>
2022-01-02 23:19:08 -0600

    websocket folder

---
+++ web-socket/app.js
@@ -0,0 +1,7 @@
+const Server = require('./server.js');
+
+
+const server = new Server();
+
+
+server.listen();
---
+++ web-socket/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 8081;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+
+        this.paths = {
+            auth: '/api/auth',
+        }
+
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use(cors());
+
+        // Directorio Público
+        this.app.use(express.static('public'));
+
+    }
+
+    routes() {
+
+        //this.app.use( this.paths.auth, require('../routes/auth'));        
+
+    }
+
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
