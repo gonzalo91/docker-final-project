38acc99ef8c7010a6a6e70ab32aba82ac7fd6e41
Gonzalo Cavazos <gonzalo.adriancr@gmail.com>
2022-02-17 10:55:49 -0600

    Fix: status indicator Loans

--- back-end/app/Repositories/Impl/LoanRepositoryImpl.php
+++ back-end/app/Repositories/Impl/LoanRepositoryImpl.php
@@ -1,18 +1,19 @@
 <?php
 namespace App\Repositories\Impl;
 
+use App\Models\OrderStatuses;
 use App\Models\{Loan, LoanStatuses};
 use App\Repositories\LoanRepository;
-use Illuminate\Support\Facades\Log;
 
 class LoanRepositoryImpl implements LoanRepository{
 
 
     function getAllLoansToFund(int $userId){
 
-        return Loan::withCount(['orders' => fn($qb) => $qb->where('user_id', $userId) ])
-                ->withSum('orders_accepted', 'real_fund')
-                ->where('status', LoanStatuses::Funding)->get();
+        return Loan::withCount([
+            'orders' => fn($qb) => $qb->where('user_id', $userId)->where('status', OrderStatuses::Accepted) 
+        ])->withSum('orders_accepted', 'real_fund')
+        ->where('status', LoanStatuses::Funding)->get();
 
     }
 
